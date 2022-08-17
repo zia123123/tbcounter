bec68a04f19d7436088297223710d6876cec74c0
zia123123 <ziamuhamad@gmail.com>
2022-08-17 16:38:10 +0700

    add nameEvent

--- app/server.js
+++ app/server.js
@@ -8,7 +8,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 const { sequelize } = require('./models/index');
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 // Settings
 const PORT = process.env.PORT || 9001;
